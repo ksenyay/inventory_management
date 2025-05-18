b1609c9bc438e7ae911d7787f1d941dfee6ac4eb
ksenyay <ksenyausachova@gmail.com>
2025-05-18 13:44:12 +0300

    Created home page and form html/css

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,7 +1,21 @@
-const index = (req, res) => {
+const categories = [
+  "Indoor Plants",
+  "Outdoor Plants",
+  "Succulents & Cacti",
+  "Flowering Plants",
+  "Herbs & Edible Plants",
+  "Trees & Large Plants",
+];
+
+const index_get = (req, res) => {
   res.render("index");
 };
 
+const new_product_get = (req, res) => {
+  res.render("form", { categories: categories });
+};
+
 module.exports = {
-  index,
+  index_get,
+  new_product_get,
 };
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const inventoryController = require("../controllers/inventoryController");
 
-router.get("/", inventoryController.index);
+router.get("/", inventoryController.index_get);
+router.get("/new", inventoryController.new_product_get);
 
 module.exports = router;
