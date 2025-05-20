6a4d045c14cfbcf2e9480a5d05fc4534e633c451
ksenyay <ksenyausachova@gmail.com>
2025-05-20 18:44:15 +0300

    Deployment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const routes = require("./routes/inventoryRoutes");
+require("dotenv").config();
 
 // Initiate an express app
 const app = express();
