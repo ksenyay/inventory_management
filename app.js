beab2ecb747ff459bed6af5e26a9c803a2b4e58e
ksenyay <ksenyausachova@gmail.com>
2025-05-23 23:38:52 +0300

    Changed the db setup on start

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const pool = require("./models/pool");
 const routes = require("./routes/inventoryRoutes");
 require("dotenv").config();
 
@@ -18,8 +19,23 @@ app.set("view engine", "ejs");
 // Set routing
 app.use("/", routes);
 
+// Database setup
+
+async function initDb() {
+  try {
+    await pool.query(
+      "ALTER TABLE products ALTER COLUMN price TYPE NUMERIC(5, 2)"
+    );
+  } catch (err) {
+    console.error("Error updating DB schema:", err);
+  }
+}
+
 // Listening to the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`The server is running at http://localhost:${PORT}`);
+
+initDb().then(() => {
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`The server is running at http://localhost:${PORT}`);
+  });
 });
